921fcb4ea1f13025213baa73ba3e6ba1dc488953
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-05 07:57:10 +0300

    Adds update

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+function handleToggle(){
+  setIsOn((turnOn) => !turnOn)
+}
+
+const color = isOn ? `red` : `white`
+
+  return <button style={{backgroundColor: color}} onClick={handleToggle}>{isOn? `ON` : `OFF`}</button>;
 }
 
 export default Toggle;
